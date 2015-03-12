acf2238a85bea23c59cbb9263ab00dbeacea8e1b
Daniel Hirsch <dlhirsch@users.noreply.github.com>
2015-03-12 15:02:50 -0700

    Created a db in the on create method in the MainActiviy class. This will provide a starting point for this project.

--- MovieQuiz/app/src/main/java/cs122b/moviequiz/MainActivity.java
+++ MovieQuiz/app/src/main/java/cs122b/moviequiz/MainActivity.java
@@ -1,9 +1,13 @@
 package cs122b.moviequiz;
 
-import android.support.v7.app.ActionBarActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,55 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase myDB= null;
+
+        try {
+            myDB = this.openOrCreateDatabase("mydb", MODE_PRIVATE, null);
+
+            /* Create a Table in the Database. */
+            myDB.execSQL("CREATE TABLE IF NOT EXISTS "
+                    + "movies"
+                    + " ( id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR, year INTEGER, director VARCHAR);");
+
+            /* Insert data to a Table*/
+            myDB.execSQL("INSERT INTO "
+                    + "movies"
+                    + " (title, year, director)"
+                    + " VALUES ('Moonrise Kingdom', 2012, 'Wes Anderson');");
+
+            /*retrieve data from database */
+            Cursor c = myDB.rawQuery("SELECT * FROM movies", null);
+
+            int Column1 = c.getColumnIndex("title");
+            int Column2 = c.getColumnIndex("year");
+            int Column3 = c.getColumnIndex("director");
+
+            // Check if our result was valid.
+            c.moveToFirst();
+            if (c != null) {
+                // Loop through all Results
+                do {
+                    String title = c.getString(Column1);
+                    int year = c.getInt(Column2);
+                    String director = c.getString(Column3);
+                    String data = title + "/" + year + "/" + director + "\n";
+
+                    LinearLayout lView = new LinearLayout(this);
+
+                    TextView myText = new TextView(this);
+                    myText.setText(data);
+
+                    lView.addView(myText);
+
+                    setContentView(lView);
+
+                } while (c.moveToNext());
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -37,3 +90,7 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
+
+
