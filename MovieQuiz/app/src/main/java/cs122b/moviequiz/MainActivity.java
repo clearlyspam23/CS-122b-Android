25cc86431f8a6f03946fe05428a2e6a4b8ea4859
Daniel Hirsch <dlhirsch@users.noreply.github.com>
2015-03-15 15:14:12 -0700

    onCreate() now creates the stars, movies, and stars_in_movies tables and successfully populates them.

--- MovieQuiz/app/src/main/java/cs122b/moviequiz/MainActivity.java
+++ MovieQuiz/app/src/main/java/cs122b/moviequiz/MainActivity.java
@@ -9,6 +9,10 @@ import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -22,45 +26,36 @@ public class MainActivity extends ActionBarActivity {
         try {
             myDB = this.openOrCreateDatabase("mydb", MODE_PRIVATE, null);
 
-            /* Create a Table in the Database. */
-            myDB.execSQL("CREATE TABLE IF NOT EXISTS "
-                    + "movies"
-                    + " ( id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR, year INTEGER, director VARCHAR);");
+            /* CREATE tables and IMPORT data */
+            importMoviesTable(myDB); // Create 'movies' table and import all data
+            importStarsTable(myDB); // Create 'stars' table and import all data
+            importStarsInMoviesTable(myDB); // Create 'stars_in_movies' table and import all data
 
-            /* Insert data to a Table*/
-            myDB.execSQL("INSERT INTO "
-                    + "movies"
-                    + " (title, year, director)"
-                    + " VALUES ('Moonrise Kingdom', 2012, 'Wes Anderson');");
+            Cursor movieCursor = myDB.rawQuery("select count(*) from movies", null);
+            movieCursor.moveToFirst();
+            int movieCount = movieCursor.getInt(0);
+            movieCursor.close();
 
-            /*retrieve data from database */
-            Cursor c = myDB.rawQuery("SELECT * FROM movies", null);
+            Cursor starsCursor = myDB.rawQuery("select count(*) from stars", null);
+            starsCursor.moveToFirst();
+            int starsCount = starsCursor.getInt(0);
+            starsCursor.close();
 
-            int Column1 = c.getColumnIndex("title");
-            int Column2 = c.getColumnIndex("year");
-            int Column3 = c.getColumnIndex("director");
+            Cursor simCursor = myDB.rawQuery("select count(*) from stars_in_movies", null);
+            simCursor.moveToFirst();
+            int simCount = simCursor.getInt(0);
+            simCursor.close();
 
-            // Check if our result was valid.
-            c.moveToFirst();
-            if (c != null) {
-                // Loop through all Results
-                do {
-                    String title = c.getString(Column1);
-                    int year = c.getInt(Column2);
-                    String director = c.getString(Column3);
-                    String data = title + "/" + year + "/" + director + "\n";
+            LinearLayout lView = new LinearLayout(this);
 
-                    LinearLayout lView = new LinearLayout(this);
+            TextView myText = new TextView(this);
 
-                    TextView myText = new TextView(this);
-                    myText.setText(data);
+            String dbcount = "Movies: " + movieCount + " Stars: " + starsCount + " SIM:" +simCount;
+            myText.setText(dbcount);
 
-                    lView.addView(myText);
+            lView.addView(myText);
 
-                    setContentView(lView);
-
-                } while (c.moveToNext());
-            }
+            setContentView(lView);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -89,7 +84,105 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void importStarsTable(SQLiteDatabase myDB) {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS stars( " +
+                                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                    "first_name VARCHAR NOT NULL, " +
+                                    "last_name VARCHAR NOT NULL, " +
+                                    "dob VARCHAR" +
+                                ");";
+
+        String insertRowTemplate = "INSERT INTO stars (id, first_name, last_name, dob) VALUES (@TABLE_VALUES@);";
+
+        String fileName = "stars.csv";
+
+        String tableName = "stars";
+
+        createAndImportTable(createTableSQL, insertRowTemplate, fileName, tableName, myDB);
+    }
+
+    private void importMoviesTable(SQLiteDatabase myDB) {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS movies( " +
+                                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                    "title VARCHAR NOT NULL, " +
+                                    "year INTEGER NOT NULL, " +
+                                    "director VARCHAR NOT NULL" +
+                                ");";
+
+        String insertRowTemplate = "INSERT INTO movies (id, title, year, director) VALUES (@TABLE_VALUES@);";
+
+        String fileName = "movies.csv";
+
+        String tableName = "movies";
+
+        createAndImportTable(createTableSQL, insertRowTemplate, fileName, tableName, myDB);
+    }
+
+    private void importStarsInMoviesTable(SQLiteDatabase myDB) {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS stars_in_movies( " +
+                                    "star_id INTEGER NOT NULL, " +
+                                    "movie_id INTEGER NOT NULL," +
+                                    "FOREIGN KEY(star_id) REFERENCES stars(id)," +
+                                    "FOREIGN KEY(movie_id) REFERENCES movies(id)" +
+                                ");";
+
+        String insertRowTemplate = "INSERT INTO stars_in_movies (star_id, movie_id) VALUES (@TABLE_VALUES@);";
+
+        String fileName = "stars_in_movies.csv";
+
+        String tableName = "stars_in_movies";
+
+        createAndImportTable(createTableSQL, insertRowTemplate, fileName, tableName, myDB);
+    }
+
+    private void createAndImportTable(String createSQL, String importTemplateSQL, String fileName, String tableName, SQLiteDatabase myDB) {
+        try {
+            /* Create movie table in the Database. */
+            myDB.execSQL(createSQL);
+
+            /* Check if data has previously been imported in onCreate() */
+            Cursor cursor = myDB.rawQuery("select count(*) from " + tableName, null);
+            cursor.moveToFirst();
+            int count = cursor.getInt(0);
+            cursor.close();
+
+            // Only insert the rows if they are not already in the db
+            if(count == 0) {
+                /* Insert data into 'movies' Table*/
+                StringBuffer movieImport = new StringBuffer();
+
+                BufferedReader reader = null;
+                try {
+                    reader = new BufferedReader(
+                            new InputStreamReader(getAssets().open(fileName)));
+
+                    String mLine = reader.readLine();
+                    while (mLine != null) {
+                        //insert row into db
+                        myDB.execSQL(importTemplateSQL.replace("@TABLE_VALUES@", mLine));
+
+                        mLine = reader.readLine();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
 
 
 
